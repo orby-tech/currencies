7cd36747194e6b7abead6483aa054291a1bb5f7b
Timur Bondarenko <tibode495@gmail.com>
2023-05-02 12:44:29 +0600

    add home page

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ApiModule],
+  imports: [
+    ApiModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../assets'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
